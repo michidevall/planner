9fbbe7a4bd98eaf4a2e41099d5dc8bb90e45b822
michidevall <glitzpixy1@yahoo.com>
2021-12-18 18:24:26 -0700

    first commit

---
+++ src/database.py
@@ -0,0 +1,22 @@
+import csv
+from src.reminder import PoliteReminder
+
+def list_reminders():
+    f = open("reminders.csv", "r")
+
+    with f:
+        reader = csv.reader(f)
+
+        for row in reader:
+            print()
+            for e in row:
+                print(e.ljust(32), end=' ')
+        print()
+
+def add_reminder(text):
+    reminder = PoliteReminder(text)
+
+
+    with open('reminders.csv', 'a+', newline='\n') as file:
+        writer = csv.writer(file)
+        writer.writerow([reminder.text])
